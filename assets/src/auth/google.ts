3ed6aa30accc486eccdb03c9f0263ea25ec894fc
begedin <begedinnikola@gmail.com>
2021-03-20 19:47:09 +0100

    Use env

--- assets/src/auth/google.ts
+++ assets/src/auth/google.ts
@@ -1,9 +1,10 @@
+/* global process */
+
 import GAuth from 'vue3-google-auth';
 const { createGAuth, useGAuth } = GAuth;
 
-export const CLIENT_ID =
-  '369380962042-4bri73mpdad3t9u89i5e44uj4cvrh1ta.apps.googleusercontent.com';
-export const CLIENT_SECRET = 'h3lOeFwNCD49qCwCb4yVXKbS';
+export const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 export const gAuth = createGAuth({
   clientId: CLIENT_ID,
